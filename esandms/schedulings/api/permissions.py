5da8c0065a2e95b2246f91a74112e66ed602d54a
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-07 12:57:11 +0300

    sınav cizelgesine sadece ogrenciler erisebilir

--- esandms/schedulings/api/permissions.py
+++ esandms/schedulings/api/permissions.py
@@ -20,6 +20,12 @@ class IsSuperUserOrReadOnly(permissions.BasePermission):
 
 class IsOwner(permissions.BasePermission):
 
-    def has_permission(self, request, viewn):
-        return request.user.is_authenticated
+    message = "Sadece öğrenciler erişim sağlayabilir"
+
+    def has_permission(self, request, view):
+        import ipdb;ipdb.set_trace()
+        user = User.objects.filter(username=request.user)
+        if request.user.is_authenticated and user.get().is_staff==False and user.get().is_superuser==False:
+            return True
+        return False
     
