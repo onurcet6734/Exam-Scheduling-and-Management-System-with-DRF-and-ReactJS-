f31b6f5dae4f76ac1cb72dc889cbfa6623da6023
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-19 16:55:54 +0300

    localstorage a token alındı

--- esandms/schedulings/api/permissions.py
+++ esandms/schedulings/api/permissions.py
@@ -23,9 +23,8 @@ class IsOwner(permissions.BasePermission):
     message = "Sadece öğrenciler erişim sağlayabilir"
 
     def has_permission(self, request, view):
-        # import ipdb;ipdb.set_trace()
         user = User.objects.filter(username=request.user)
-        if request.user.is_authenticated and user.get().is_staff==False and user.get().is_superuser==False:
+        if request.user.is_authenticated and user.get().is_staff==True and user.get().is_superuser==False:
             return True
         return False
     
--- esandms/schedulings/api/views.py
+++ esandms/schedulings/api/views.py
@@ -9,6 +9,8 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token, TokenProxy
 from rest_framework import viewsets
+from django.contrib.auth.hashers import make_password
+
 
 
 
