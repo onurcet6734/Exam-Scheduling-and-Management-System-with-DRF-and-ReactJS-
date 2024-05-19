ff16b21f9d36e68aeda3ed6600095099a02c99db
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-19 18:23:51 +0300

    sınav takvimi dinamik alındı

--- esandms/exams/api/serializers.py
+++ esandms/exams/api/serializers.py
@@ -1,17 +1,30 @@
 from rest_framework import serializers
 from exams.models import Exam
+from classes.models import Classes
+
+class ClassNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classes
+        fields = ['name']
 
 class ExamsSerializer(serializers.ModelSerializer):
+    classid = serializers.PrimaryKeyRelatedField(queryset=Classes.objects.all(), write_only=True)
+    class_info = ClassNameSerializer(source='classid', read_only=True)
+
     class Meta:
         model = Exam
-        fields = '__all__'
+        fields = ['id', 'created_on', 'updated_on', 'name', 'classid', 'class_info']
 
     def create(self, validated_data):
         return super().create(validated_data)
     
     def update(self, instance, validated_data):
-        instance.classid = validated_data.get('classid', instance.classid)
+        classid_data = validated_data.pop('classid')
+        classid = instance.classid
+
+        classid.name = classid_data.get('name', classid.name)
+        classid.save()
+
         instance.name = validated_data.get('name', instance.name)
         instance.save()
         return instance
-    
--- esandms/schedulings/api/serializers.py
+++ esandms/schedulings/api/serializers.py
@@ -24,10 +24,20 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
-        
 
 class SchedulingSerializer(serializers.ModelSerializer):
+    classid = serializers.PrimaryKeyRelatedField(queryset=Classes.objects.all(), write_only=True)
+    class_info = ClassesSerializer(source='classid', read_only=True)
+
+    hallid = serializers.PrimaryKeyRelatedField(queryset=Hall.objects.all(), write_only=True)
+    hall_info = HallSerializer(source='hallid', read_only=True)
+
+    examid = serializers.PrimaryKeyRelatedField(queryset=Exam.objects.all(), write_only=True)
+    exam_info = ExamSerializer(source='examid', read_only=True)
+
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
+    user_info = UserSerializer(source='user', read_only=True)
 
     class Meta:
         model = Scheduling
-        fields = '__all__'
+        fields = ['id', 'created_on', 'updated_on', 'school_number', 'exam_start_date', 'exam_finish_date', 'duration', 'classid', 'class_info', 'hallid', 'hall_info', 'examid', 'exam_info', 'user', 'user_info']
