08bce3699f1ff12917bd0443636ed2c44cd95b76
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-19 15:33:04 +0300

    user için params eklendi

--- esandms/schedulings/api/views.py
+++ esandms/schedulings/api/views.py
@@ -52,3 +52,10 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(username=username)
+        return queryset
