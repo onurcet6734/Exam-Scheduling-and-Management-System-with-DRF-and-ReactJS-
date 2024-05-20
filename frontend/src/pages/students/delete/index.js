c772bf82b002f7b88fd16b7bb9fa64844afbf5d2
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-20 23:16:42 +0300

    student tamamen bitti

--- frontend/src/pages/students/delete/index.js
+++ frontend/src/pages/students/delete/index.js
@@ -1,52 +1,67 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import Header from "../../../components/header";
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import axios from "axios";
 
-const StudentsDelete = () => {
+const StudentsDelete = (props) => {
+
+    const [item, setItem] = useState({});
+    const token = localStorage.getItem('token');
+
+
+    useEffect(() => {
+        setItem(props.data);
+    }, [props.data])
+
+    const handleDelete = () => {
+        axios.delete(`https://api.qrdestek.com/users/${item.id}/`, {
+            headers: {
+            Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => {
+
+            NotificationManager.success('Success message', 'This data is successfully deleted.');
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+
     return (
         <>
             <div className="flex">
                 <Header />
 
                 <div className="items-center w-full mx-32">
-                    <p className="my-4 px-2 text-2xl font-bold tracking-wider">Create</p>
+                    <p className="my-4 px-2 text-2xl font-bold tracking-wider">Delete</p>
+                    <p className="text-lg py-2 font-medium tracking-wider border-b-4">Are you sure you want to delete this?</p>
                     <p className="text-lg py-2 font-medium tracking-wider border-b-4">Student</p>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Name</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                    <div className="w-1/3 my-4 flex">
+                        <p className="text-md font-medium w-full">Name</p>
+                        <p className="text-md font-base">{item?.first_name}</p>
                     </div>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Surname</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                    <div className="w-1/3 my-4 flex">
+                        <p className="text-md font-medium w-full">Surname</p>
+                        <p className="text-md font-base">{item?.last_name}</p>
                     </div>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">SchoolNumber</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                    <div className="w-1/3 my-4 flex">
+                        <p className="text-md font-medium w-full">Username</p>
+                        <p className="text-md font-base">{item?.username}</p>
                     </div>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Number Of Students</p>
-                        <input className="w-full" placeholder="Please input the number of seats..." />
-                    </div>
-
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Class</p>
-                        <input className="w-full" placeholder="Please input the number of seats..." />
-                    </div>
-
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Login</p>
-                        <input className="w-full" placeholder="Please input the number of seats..." />
-                    </div>
-
-                    <button className="bg-blue-600 text-white py-2 px-4 rounded-lg">Save</button>
+                   
+                    <button className="bg-blue-600 text-white py-2 px-4 rounded-lg" onClick={() => handleDelete()}>Delete</button>
 
                     <div className="pt-2">
-                        <Link to="/" className="text-blue-600">Back to List</Link>
+                        <Link to="/students" onClick={() => window.location.href="/students"} className="text-blue-600">Back to List</Link>
                     </div>
+
+                    <NotificationContainer />
                 </div>
             </div>
         </>
--- frontend/src/pages/students/detail/index.js
+++ frontend/src/pages/students/detail/index.js
@@ -1,51 +1,60 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import Header from "../../../components/header";
+import axios from "axios";
 
-const StudentsDetail = () => {
+const StudentsDetail = (props) => {
+
+    const [item, setItem] = useState({});
+
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        // const getStudentData = (token) => {
+        //     axios.get(`https://api.qrdestek.com/users/`, {
+        //         headers: {
+        //             Authorization: `Bearer ${token}`
+        //         }
+        //     })
+        //     .then(res => {
+        //         setStudentData(res.data);
+        //     })
+        //     .catch(error => {
+        //         console.error(error);
+        //     });
+        // }
+
+        setItem(props.data);
+        // getStudentData(token);
+    }, [props.data])
+
     return (
         <>
             <div className="flex">
                 <Header />
 
                 <div className="items-center w-full mx-32">
-                    <p className="my-4 px-2 text-2xl font-bold tracking-wider">Create</p>
+                    <p className="my-4 px-2 text-2xl font-bold tracking-wider">Detail</p>
                     <p className="text-lg py-2 font-medium tracking-wider border-b-4">Student</p>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Name</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                    <div className="w-1/3 my-4 flex">
+                        <p className="text-md font-medium w-full">Name</p>
+                        <p className="text-md font-base">{item?.first_name}</p>
                     </div>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Surname</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                    <div className="w-1/3 my-4 flex">
+                        <p className="text-md font-medium w-full">Surname</p>
+                        <p className="text-md font-base">{item?.last_name}</p>
                     </div>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">SchoolNumber</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                    <div className="w-1/3 my-4 flex">
+                        <p className="text-md font-medium w-full">Username</p>
+                        <p className="text-md font-base">{item?.username}</p>
                     </div>
 
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Number Of Students</p>
-                        <input className="w-full" placeholder="Please input the number of seats..." />
-                    </div>
-
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Class</p>
-                        <input className="w-full" placeholder="Please input the number of seats..." />
-                    </div>
-
-                    <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">Login</p>
-                        <input className="w-full" placeholder="Please input the number of seats..." />
-                    </div>
-
-                    <button className="bg-blue-600 text-white py-2 px-4 rounded-lg">Save</button>
 
                     <div className="pt-2">
-                        <Link to="/" className="text-blue-600">Back to List</Link>
+                        <Link to="/students"  onClick={() => window.location.href="/students"} className="text-blue-600">Back to List</Link>
                     </div>
                 </div>
             </div>
