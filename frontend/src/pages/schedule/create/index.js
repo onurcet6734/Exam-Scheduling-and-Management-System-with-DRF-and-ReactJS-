8ab0e3eeb74790bbd9a8899af6ee0893959d6e58
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-21 00:22:07 +0300

    fe dosyası gonderildi

--- frontend/src/pages/schedule/create/index.js
+++ frontend/src/pages/schedule/create/index.js
@@ -1,8 +1,145 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Header from "../../../components/header";
 
+import axios from "axios";
+
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+
+import DatePicker from "react-datepicker";
+import TimePicker from 'react-time-picker';
+
+import 'react-time-picker/dist/TimePicker.css';
+import 'react-clock/dist/Clock.css';
+
+import "react-datepicker/dist/react-datepicker.css";
+
 const ScheduleCreate = () => {
+    const [examDate, setExamDate] = useState(new Date());
+    const [examTime, setExamTime] = useState('10:00');
+    const [hall, setHall] = useState({});
+    const [exam, setExam] = useState({});
+    const [duration, setDuration] = useState("");
+    const [student, setStudent] = useState({});
+    const [hallsData, setHallsData] = useState([]);
+    const [examsData, setExamsData] = useState([]);
+    const [studentData, setStudentData] = useState([]);
+    const [classData, setClassData] = useState([]);
+    const [classItem, setClassItem] = useState({});
+    const [cookies, setCookie] = useCookies(['name']);
+
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const getHall = (token) => {
+            axios.get(`https://api.qrdestek.com/api/hall/list-create/`, {
+                headers: {
+                Authorization: `Bearer ${token}`
+                }
+            })
+            .then(res => {
+                // setSchedule(res.data);
+                setHallsData(res.data);
+                setHall(res.data[0]);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+
+        const getExamData = (token) => {
+            axios.get(`https://api.qrdestek.com/api/exam/list-create/`, {
+                headers: {
+                Authorization: `Bearer ${token}`
+                }
+            })
+            .then(res => {
+                // setSchedule(res.data);
+                setExamsData(res.data);
+                setExam(res.data[0]);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+
+        const getStudentSchedule = (token) => {
+            axios.get(`https://api.qrdestek.com/users/?is_superuser=false`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then(res => {
+                setStudentData(res.data);
+                setStudent(res.data[0])
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+
+        const getClassData = (token) => {
+            axios.get(`https://api.qrdestek.com/api/class/list-create/`, {
+                headers: {
+                Authorization: `Bearer ${token}`
+                }
+            })
+            .then(res => {
+                // setSchedule(res.data);
+                setClassData(res.data);
+                setClassItem(res.data[0]);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+
+
+        getHall(token);
+        getExamData(token);
+        getStudentSchedule(token);
+        getClassData(token);
+    }, [])
+
+    const handleCreate = () => {
+
+        classData.map((item, index) => {
+            if (item.name == exam.class_info.name) {
+                setClassItem(item);
+                let d = 0;
+
+                const data = {
+                    school_number: 2,
+                    exam_start_date: examDate.getFullYear() + "-" + examDate.getMonth() + "-" + examDate.getDay() + "T" + examTime + ":00",
+                    exam_finish_date: examDate.getFullYear() + "-" + examDate.getMonth() + "-" + examDate.getDay() + "T" + examTime + ":00",
+                    classid: item.id,
+                    class_info: item,
+                    hallid: hall.id,
+                    hall_info: hall,
+                    examid: exam.id,
+                    exam_info: exam,
+                    user: student.id,
+                    user_info: student
+                }
+
+                axios.post(`https://api.qrdestek.com/api/scheduling/list-create/`, data, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                .then(res => {
+                    // setSchedule(res.data);
+                    // console.log(res, state);
+                    // setHallData(res.data);
+                    NotificationManager.success('Success Message', 'This data is successfully created.');
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            }
+        })
+    }
+
     return (
         <>
             <div className="flex">
@@ -13,45 +150,76 @@ const ScheduleCreate = () => {
                     <p className="text-lg py-2 font-medium tracking-wider border-b-4">Schedule</p>
 
                     <div className="w-1/3 my-4">
-                        <p className="text-md font-medium">PaperName</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                        <p className="text-md font-medium">School Number</p>
+                        <input className="w-full" placeholder="Please input the school Number..." />
                     </div>
 
                     <div className="w-1/3 my-4">
                         <p className="text-md font-medium">ExamDate</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                        <DatePicker selected={examDate} onChange={(date) => setExamDate(date)} dateFormat="MM/dd/yyyy" />
                     </div>
 
                     <div className="w-1/3 my-4">
                         <p className="text-md font-medium">ExamTime</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                        <TimePicker onChange={(time) => setExamTime(time)} value={examTime} />
                     </div>
 
                     <div className="w-1/3 my-4">
                         <p className="text-md font-medium">Duration</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                        <input className="w-full" type="number" placeholder="Please input the duration..." onChange={(e) => setDuration(e.target.value)} />
                     </div>
 
                     <div className="w-1/3 my-4">
                         <p className="text-md font-medium">Hall</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                        <select className="w-full" onChange={(e) => setHall(hallsData[e.target.value])}>
+                            {hallsData.map((item, index) => {
+                                return (
+                                    <option value={index} key={item.id}>{item.name}</option>
+                                )
+                            })}
+                        </select>
                     </div>
 
                     <div className="w-1/3 my-4">
                         <p className="text-md font-medium">Exam</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                        <select className="w-full" onChange={(e) => setExam(examsData[e.target.value])}>
+                            {examsData.map((item, index) => {
+                                return (
+                                    <option value={index} key={item.id}>{item.name}</option>
+                                )
+                            })}
+                        </select>
                     </div>
 
                     <div className="w-1/3 my-4">
                         <p className="text-md font-medium">Student</p>
-                        <input className="w-full" placeholder="Please input the name..." />
+                        <select className="w-full" onChange={(e) => setStudent(studentData[e.target.value])}>
+                            {studentData.map((item, index) => {
+                                return (
+                                    <option value={index} key={item.id}>{item.username}</option>
+                                )
+                            })}
+                        </select>
                     </div>
 
-                    <button className="bg-blue-600 text-white py-2 px-4 rounded-lg">Save</button>
+                    <div className="w-1/3 my-4">
+                        <p className="text-md font-medium">Exam</p>
+                        <select className="w-full" onChange={(e) => setExam(classData[e.target.value])}>
+                            {examsData.map((item, index) => {
+                                return (
+                                    <option value={index} key={item.id}>{item.name}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+
+                    <button className="bg-blue-600 text-white py-2 px-4 rounded-lg" onClick={() => handleCreate()}>Save</button>
 
                     <div className="pt-2">
-                        <Link to="/" className="text-blue-600">Back to List</Link>
+                        <Link to="/schedule" onClick={() => window.location.href="/schedule"} className="text-blue-600">Back to List</Link>
                     </div>
+
+                    <NotificationContainer />
                 </div>
             </div>
         </>
--- frontend/src/pages/schedule/index.js
+++ frontend/src/pages/schedule/index.js
@@ -1,86 +1,163 @@
 //node modules
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserPlus, faSearch, faReply, faEdit, faRecycle, faI } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 //custom component
 import Header from "../../components/header";
 
+import ScheduleDelete from "./delete";
+import ScheduleEdit from "./edit";
+import ScheduleDetail from "./detail";
+
 const Schedule = () => {
+    const [token, setToken] = useState("");
+    const [scheduleData, setScheduleData] = useState([]);
+    const [state, setState] = useState(0);
+    const [selectItem, setSelectItem] = useState({});
+    const [examTime, setExamTime] = useState("");
+    const [duration, setDuration] = useState("");
+
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        const getExamData = (token) => {
+            axios.get(`https://api.qrdestek.com/api/scheduling/list-create/`, {
+                headers: {
+                Authorization: `Bearer ${token}`
+                }
+            })
+            .then(res => {
+                // setSchedule(res.data);
+                // console.log(res);
+                setScheduleData(res.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+
+        if (!storedToken) {
+            window.location.href="/";
+        }
+        else {
+            // console.log(cookies.token);
+            setToken(storedToken)
+        }
+
+        getExamData(storedToken);
+    }, [])
+
+    const handleEdit = (item) => {
+        setState(1);
+        setSelectItem(item);
+    }
+
+    const handleDetail = (item) => {
+        setState(2);
+        setSelectItem(item);
+    }
+
+    const handleDelete = (item) => {
+        setState(3);
+        setSelectItem(item);
+    }
+
+
     return (
         <>
-            <div className="flex">
-                <Header />
+            {state == 0 && (
+                <div className="flex">
+                    <Header />
 
-                <div className="w-screen">
-                    <p className="text-3xl font-bold p-10">Scheduling Management</p>
+                    <div className="w-screen">
+                        <p className="text-3xl font-bold p-10">Scheduling Management</p>
 
-                    <Link to="/schedule/create" onClick={() => window.location.href="/schedule/create"}>
-                        <div className="px-6 flex">
-                            <p className="text-lg font-medium">Create new</p>
-                            <FontAwesomeIcon icon={faUserPlus} className="pt-2 px-4" />
-                        </div>
-                    </Link>
+                        <Link to="/schedule/create" onClick={() => window.location.href="/schedule/create"}>
+                            <div className="px-6 flex">
+                                <p className="text-lg font-medium">Create new</p>
+                                <FontAwesomeIcon icon={faUserPlus} className="pt-2 px-4" />
+                            </div>
+                        </Link>
 
-                    <div className="pt-6">
-                        <div className="py-4 px-6 flex">
-                            <input placeholder="Search Name..."/>
+                        <div className="pt-6">
+                            <div className="py-4 px-6 flex">
+                                <input placeholder="Search Name..."/>
 
-                            <button type="primary" className="bg-sky-500 px-3 h-10 rounded-lg text-white">
-                                <div className="flex">
-                                    Search <FontAwesomeIcon icon={faSearch} className="pt-1 px-2" />
-                                </div>
-                            </button>
+                                <button type="primary" className="bg-sky-500 px-3 h-10 rounded-lg text-white">
+                                    <div className="flex">
+                                        Search <FontAwesomeIcon icon={faSearch} className="pt-1 px-2" />
+                                    </div>
+                                </button>
 
-                            <button type="primary" className="bg-slate-400 px-3 h-10 rounded-lg text-white mx-8">
-                                <div className="flex">
-                                    Clear <FontAwesomeIcon icon={faReply} className="pt-1 px-2" />
-                                </div>
-                            </button>
-                        </div>
+                                <button type="primary" className="bg-slate-400 px-3 h-10 rounded-lg text-white mx-8">
+                                    <div className="flex">
+                                        Clear <FontAwesomeIcon icon={faReply} className="pt-1 px-2" />
+                                    </div>
+                                </button>
+                            </div>
 
-                        <div className="pt-4 text-sm">
-                            <table className="mx-3">
-                                <thead>
-                                    <tr className="w-screen border-b-4">
-                                        <th className="pr-48">Paper Name</th>
-                                        <th className="pr-48">Exam Data</th>
-                                        <th className="pr-48">Exam Time</th>
-                                        <th className="pr-48">Duration</th>
-                                        <th className="pr-48">Name of Hall</th>
-                                        <th className="pr-48">Name of Exam</th>
-                                        <th className="pr-48">Name of Student</th>
-                                        <th className="pr-48">Name of Class</th>
-                                        <th className="w-full"></th>
-                                    </tr>
-                                </thead>
+                            <div className="pt-4 text-sm">
+                                <table className="mx-3">
+                                    <thead>
+                                        <tr className="w-screen border-b-4">
+                                            <th className="pr-48">School Number</th>
+                                            <th className="pr-48">Exam Start Date</th>
+                                            <th className="pr-48">Exam End Date</th>
+                                            <th className="pr-48">Duration</th>
+                                            <th className="pr-48">Name of Hall</th>
+                                            <th className="pr-48">Name of Exam</th>
+                                            <th className="pr-48">Name of Student</th>
+                                            <th className="pr-48">Name of Class</th>
+                                            <th className="w-full"></th>
+                                        </tr>
+                                    </thead>
 
-                                <tbody>
-                                    <tr className="w-screen border-b-2">
-                                        <td className="py-2">Special Topics</td>
-                                        <td className="py-2">08.06 2023</td>
-                                        <td className="py-2">14:00:00</td>
-                                        <td className="py-2">60</td>
-                                        <td className="py-2">T-356</td>
-                                        <td className="py-2">Special Topics</td>
-                                        <td className="py-2">Occer</td>
-                                        <td className="py-2">Computer Engineering</td>
-                                        <td className="flex items-center w-full">
-                                            <div className="flex justify-center w-full">
-                                                <button className="text-white px-2 py-1"><FontAwesomeIcon icon={faEdit} className="bg-sky-500 p-2" /></button>
-                                                <button className="text-white px-2 py-1"><FontAwesomeIcon icon={faI} className="bg-teal-500 p-2" /></button>
-                                                <button className="text-white px-2 py-1"><FontAwesomeIcon icon={faRecycle} className="bg-rose-600 p-2" /></button>
-                                            </div>
+                                    <tbody>
+                                        {scheduleData.map((item, index) => {
+                                            return (
+                                                <tr className="w-screen border-b-2" key={index}>
+                                                    <td className="py-2">{item.school_number}</td>
+                                                    <td className="py-2">{item.exam_start_date}</td>
+                                                    <td className="py-2">{item.exam_finish_date}</td>
+                                                    <td className="py-2">{item.duration}</td>
+                                                    <td className="py-2">{item.hall_info.name}</td>
+                                                    <td className="py-2">{item.exam_info.name}</td>
+                                                    <td className="py-2">{item.user_info.username}</td>
+                                                    <td className="py-2">{item.class_info.name}</td>
+                                                    <td className="flex items-center w-full">
+                                                        <div className="flex justify-center w-full">
+                                                            <button className="text-white px-2 py-1" onClick={() => handleEdit(item)}><FontAwesomeIcon icon={faEdit} className="bg-sky-500 p-2" /></button>
+                                                            <button className="text-white px-2 py-1" onClick={() => handleDetail(item)}><FontAwesomeIcon icon={faI} className="bg-teal-500 p-2" /></button>
+                                                            <button className="text-white px-2 py-1" onClick={() => handleDelete(item)}><FontAwesomeIcon icon={faRecycle} className="bg-rose-600 p-2" /></button>
+                                                        </div>
+                                                        
+                                                    </td>
+                                                </tr>
+                                            )
                                             
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                                        })}
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
+            {state === 1 && (
+                <ScheduleEdit data={selectItem} />
+            )}
+
+            {state === 2 && (
+                <ScheduleDetail data={selectItem} />
+            )}
+
+            {state === 3 && (
+                <ScheduleDelete data={selectItem} />
+            )}
         </>
     )
 }
