6ef21597b404a9f099b7cd78efc9e243c1f7ebbd
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-28 01:32:04 +0300

    hall ve class testi

--- esandms/classes/tests.py
+++ esandms/classes/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from halls.models import Hall
+from halls.api.serializers import HallsSerializer
+from django.contrib.auth.models import User
+import json
+from esandms.jwt_authentication import authenticate_user 
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class ClassesAPITestCase(TestCase):
+
+    list_create_url = "/api/class/list-create/"
+    detail_url = "/api/class/update-delete/22/"
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.username = 'uniproject'
+        self.password = 'unisupport23'
+        self.user = User.objects.create_superuser(username=self.username, password=self.password)
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self):
+        is_authenticated = authenticate_user(self.client, self.username, self.password)
+        self.assertTrue(is_authenticated)
+
+    def test_classes_list_create_view_get(self):
+        response = self.client.get(self.list_create_url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_classes_list_create_view_post(self):
+        response = self.client.post(self.list_create_url, json.dumps({ "name": "Elektrik Mühendisliği2", "year": 3, "semester": "İlkbahar", "count_of_students": 625 }), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Elektrik Mühendisliği2')
+        self.assertEqual(response.data['year'], 3)
+
+
+
--- esandms/halls/tests.py
+++ esandms/halls/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from halls.models import Hall
+from halls.api.serializers import HallsSerializer
+from django.contrib.auth.models import User
+import json
+from esandms.jwt_authentication import authenticate_user 
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class HallsAPITestCase(TestCase):
+
+    list_create_url = "https://api.qrdestek.com/api/hall/list-create/"
+    detail_url = "https://api.qrdestek.com/api/hall/update-delete/43/"
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.username = 'uniproject'
+        self.password = 'unisupport23'
+        self.user = User.objects.create_superuser(username=self.username, password=self.password)
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self):
+        is_authenticated = authenticate_user(self.client, self.username, self.password)
+        self.assertTrue(is_authenticated)
+
+    def test_halls_list_create_view_get(self):
+        print(self.list_create_url)
+        response = self.client.get(self.list_create_url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_halls_list_create_view_post(self):
+        response = self.client.post(self.list_create_url, json.dumps({'name': 'Test Hall 2', 'number_of_seats': 200}), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Test Hall 2')
+        self.assertEqual(response.data['number_of_seats'], 200)
+    
+
+
+    def test_halls_detail_view_put(self):
+        data = {
+            'name': 'C583',
+            'number_of_seats': 500
+        }
+        response = self.client.put("https://api.qrdestek.com/api/hall/update-delete/43", json.dumps(data), content_type='application/json')
+        self.assertEqual(301, response.status_code)
+
+    def test_halls_detail_view_delete(self):
+        response = self.client.delete("https://api.qrdestek.com/api/hall/update-delete/43")
+        self.assertEqual(301, response.status_code)
+
+ 
+
