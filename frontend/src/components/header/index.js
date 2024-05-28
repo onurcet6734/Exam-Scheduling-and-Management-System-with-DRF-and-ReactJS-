05d171fc9af9cedf81c27537c9af52401f575052
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-28 09:57:15 +0300

    beyaz oldu yazılar

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -17,36 +17,36 @@ const Header = () => {
                 <div className="mt-16 text-white">
                     <Link to="/halls" onClick={() => window.location.href="/halls"} >
                         <div className="pl-4 flex py-4 bg-sky-900 hover:bg-sky-950">
-                            <FontAwesomeIcon icon={faHouse} className="pt-1" />
-                            <p className="text-md font-medium tracking-wider px-4">Halls</p>
+                            <FontAwesomeIcon icon={faHouse} className="pt-1 text-white" />
+                            <p className="text-md font-medium tracking-wider px-4 text-white">Halls</p>
                         </div>
                     </Link>
 
                     <Link to="/students" onClick={() => window.location.href="/students"} >
                         <div className="pl-4 flex py-4 bg-sky-900 hover:bg-sky-950">
-                            <FontAwesomeIcon icon={faPeopleGroup} className="pt-1" />
-                            <p className="text-md font-medium tracking-wider px-4">Students</p>
+                            <FontAwesomeIcon icon={faPeopleGroup} className="pt-1 text-white" />
+                            <p className="text-md font-medium tracking-wider px-4 text-white">Students</p>
                         </div>
                     </Link>
 
                     <Link to="/classes" onClick={() => window.location.href="/classes"} >
                         <div className="pl-4 flex py-4 bg-sky-900 hover:bg-sky-950">
-                            <FontAwesomeIcon icon={faRestroom} className="pt-1" />
-                            <p className="text-md font-medium tracking-wider px-4">Classes</p>
+                            <FontAwesomeIcon icon={faRestroom} className="pt-1 text-white" />
+                            <p className="text-md font-medium tracking-wider px-4 text-white">Classes</p>
                         </div>
                     </Link>
 
                     <Link to="/exams" onClick={() => window.location.href="/exams"} >
                         <div className="pl-4 flex py-4 bg-sky-900 hover:bg-sky-950">
-                            <FontAwesomeIcon icon={faChalkboard} className="pt-1" />
-                            <p className="text-md font-medium tracking-wider px-4">Exams</p>
+                            <FontAwesomeIcon icon={faChalkboard} className="pt-1 text-white" />
+                            <p className="text-md font-medium tracking-wider px-4 text-white">Exams</p>
                         </div>
                     </Link>
 
                     <Link to="/schedule" onClick={() => window.location.href="/schedule"} >
                         <div className="pl-4 flex py-4 bg-sky-900 hover:bg-sky-950">
-                            <FontAwesomeIcon icon={faCalendar} className="pt-1" />
-                            <p className="text-md font-medium tracking-wider px-4">Schedule</p>
+                            <FontAwesomeIcon icon={faCalendar} className="pt-1 text-white" />
+                            <p className="text-md font-medium tracking-wider px-4 text-white">Schedule</p>
                         </div>
                     </Link>
 
@@ -59,8 +59,8 @@ const Header = () => {
                                 window.location.href = "/login"; // optional: user to be redirected to the login page
                             }}
                         >
-                            <FontAwesomeIcon icon={faLock} className="pt-1" />
-                            <p className="text-md font-medium tracking-wider px-4">Log out</p>
+                            <FontAwesomeIcon icon={faLock} className="pt-1 text-white" />
+                            <p className="text-md font-medium tracking-wider px-4 text-white">Logout</p>
                         </div>
                     </Link>
                     
