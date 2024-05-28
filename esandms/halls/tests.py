7ff184763c386844b322ce61ba67fcf504d92dae
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-05-28 09:20:43 +0300

    tst

--- esandms/halls/tests.py
+++ esandms/halls/tests.py
@@ -5,9 +5,8 @@ from halls.models import Hall
 from halls.api.serializers import HallsSerializer
 from django.contrib.auth.models import User
 import json
-from esandms.jwt_authentication import authenticate_user 
+from esandms.jwt_authentication import authenticate_user
 from rest_framework.test import APIClient
-
 
 class HallsAPITestCase(TestCase):
 
@@ -17,6 +16,7 @@ class HallsAPITestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
 
+        self.token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4MDgxNjc0LCJpYXQiOjE3MTY4NzIwNzQsImp0aSI6ImZhMWFlYWRmYTJiMTQ5ZjM4NjQ3Yzk0YTNlN2Q5Y2E3IiwidXNlcl9pZCI6NH0._Ih-mGdoDvwdv2sOriPqBpdLJcLzypDY8Uo8ZYscUis"
         self.username = 'uniproject'
         self.password = 'unisupport23'
         self.user = User.objects.create_superuser(username=self.username, password=self.password)
@@ -27,30 +27,23 @@ class HallsAPITestCase(TestCase):
         self.assertTrue(is_authenticated)
 
     def test_halls_list_create_view_get(self):
-        print(self.list_create_url)
         response = self.client.get(self.list_create_url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
 
     def test_halls_list_create_view_post(self):
         response = self.client.post(self.list_create_url, json.dumps({'name': 'Test Hall 2', 'number_of_seats': 200}), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['name'], 'Test Hall 2')
         self.assertEqual(response.data['number_of_seats'], 200)
-    
 
-
-    def test_halls_detail_view_put(self):
+    def test_halls_detail_view_patch(self):
         data = {
             'name': 'C583',
             'number_of_seats': 500
         }
-        response = self.client.put("https://api.qrdestek.com/api/hall/update-delete/43", json.dumps(data), content_type='application/json')
-        self.assertEqual(301, response.status_code)
+        response = self.client.patch(self.detail_url, json.dumps(data), content_type='application/json', HTTP_AUTHORIZATION=self.token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_halls_detail_view_delete(self):
-        response = self.client.delete("https://api.qrdestek.com/api/hall/update-delete/43")
-        self.assertEqual(301, response.status_code)
-
- 
-
+        response = self.client.delete(self.detail_url, HTTP_AUTHORIZATION=self.token)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- esandms/schedulings/tests.py
+++ esandms/schedulings/tests.py
@@ -1,3 +1,77 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from schedulings.models import Scheduling, Classes, Hall, Exam
+from esandms.jwt_authentication import authenticate_user 
+import json
+from datetime import datetime, timedelta
+import pytz
 
-# Create your tests here.
+
+def populate_schedule_detail_url(schedule_id):
+    return reverse("schedulings:scheduling-detail", kwargs={"pk": schedule_id})
+
+class SchedulingViewTestCase(TestCase):
+    """Test case for the Scheduling views"""
+
+    url_listCreate = reverse("schedulings:scheduling-list")
+
+    def setUp(self):
+        self.client = APIClient()
+        self.username = 'admin'
+        self.password = '1234'
+        self.student_user = User.objects.create_user(username='senasincanli', password='1234')
+        self.class_instance = Classes.objects.create(name='Endustri Mühendisligi',year=4,semester='Sonbahar',count_of_students=100) 
+        self.exam = Exam.objects.create(name="Software Design Applicationdas",classid=self.class_instance)
+        self.hall =  Hall.objects.get(name='T blok 450',number_of_seats=100)
+        self.start_time = datetime.now(pytz.UTC) 
+        self.end_time= self.start_time + timedelta(hours=2)
+        self.user = User.objects.create_superuser(username=self.username, password=self.password,is_superuser=True)
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self):
+        is_authenticated = authenticate_user(self.client, self.username, self.password)
+        self.assertTrue(is_authenticated)
+
+    def test_create_scheduling(self):
+        response = self.client.post(self.url_listCreate, data = {"school_number": "123456", "exam_start_date": "2023-12-12T10:00:00Z", "exam_finish_date": "2023-12-12T11:00:00Z", "classid": self.class_instance.id, "hallid": self.hall.id, "examid": self.exam.id, "user": self.student_user.id }, format='json')
+        #print response message
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_get_all_schedulings(self):
+        response = self.client.get(self.url_listCreate)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_scheduling(self):
+        scheduling = Scheduling.objects.create(
+            school_number="123456",
+            exam_start_date=self.start_time,
+            exam_finish_date=self.end_time,
+            classid=self.class_instance,
+            hallid=self.hall,
+            examid=self.exam,
+            user=self.student_user
+        )
+        schedule_data = {'school_number': 'B2005.03164132'}
+        updateDelete_url = reverse("schedulings:scheduling-detail", kwargs={"pk": scheduling.id})
+        response = self.client.patch(updateDelete_url, schedule_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_delete_scheduling(self):
+        scheduling = Scheduling.objects.create(
+            school_number="123456",
+            exam_start_date=self.start_time,
+            exam_finish_date=self.end_time,
+            classid=self.class_instance,
+            hallid=self.hall,
+            examid=self.exam,
+            user=self.student_user
+        )
+        updateDelete_url = reverse("schedulings:scheduling-detail", kwargs={"pk": scheduling.id})
+        response = self.client.delete(updateDelete_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
