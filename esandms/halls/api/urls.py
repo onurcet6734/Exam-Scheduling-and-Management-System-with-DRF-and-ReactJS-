89b5c2d795186d4765d071f4b99b9833e76ab4dd
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2024-06-07 00:56:33 +0300

    hall tarafında testler bitti

--- esandms/halls/api/urls.py
+++ esandms/halls/api/urls.py
@@ -4,6 +4,6 @@ from .views import HallsListCreateView, HallsUpdateDeleteDetailView
 app_name = "halls"
 
 urlpatterns = [
-    path('list-create/', HallsListCreateView.as_view()),
-    path('update-delete/<int:pk>/', HallsUpdateDeleteDetailView.as_view()),
+    path('list-create/', HallsListCreateView.as_view(), name='list-create'),
+    path('update-delete/<int:pk>/', HallsUpdateDeleteDetailView.as_view(), name='update-delete'),
 ]
--- esandms/halls/tests.py
+++ esandms/halls/tests.py
@@ -2,25 +2,28 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from halls.models import Hall
-from halls.api.serializers import HallsSerializer
 from django.contrib.auth.models import User
 import json
 from esandms.jwt_authentication import authenticate_user
 from rest_framework.test import APIClient
 
-class HallsAPITestCase(TestCase):
 
-    list_create_url = "https://api.qrdestek.com/api/hall/list-create/"
-    detail_url = "https://api.qrdestek.com/api/hall/update-delete/43/"
+def populate_customer_detail_url(customer_id):
+    return reverse("halls:update-delete", kwargs={"pk": customer_id})
+
+class HallsCreateAPITestCase(TestCase):
+
+    list_create_url = reverse('halls:list-create')
 
     def setUp(self):
         self.client = APIClient()
-
-        self.token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4MDgxNjc0LCJpYXQiOjE3MTY4NzIwNzQsImp0aSI6ImZhMWFlYWRmYTJiMTQ5ZjM4NjQ3Yzk0YTNlN2Q5Y2E3IiwidXNlcl9pZCI6NH0._Ih-mGdoDvwdv2sOriPqBpdLJcLzypDY8Uo8ZYscUis"
         self.username = 'uniproject'
         self.password = 'unisupport23'
         self.user = User.objects.create_superuser(username=self.username, password=self.password)
         self.test_jwt_authentication()
+        self.hall = Hall.objects.create(name='Test Hall', number_of_seats=100)
+        self.update_delete_url = reverse("halls:update-delete", kwargs={"pk": self.hall.pk})
+
 
     def test_jwt_authentication(self):
         is_authenticated = authenticate_user(self.client, self.username, self.password)
@@ -31,19 +34,19 @@ class HallsAPITestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_halls_list_create_view_post(self):
-        response = self.client.post(self.list_create_url, json.dumps({'name': 'Test Hall 2', 'number_of_seats': 200}), content_type='application/json')
+        response = self.client.post(self.list_create_url, json.dumps({'name': 'Test Hall 2', 
+                                                        'number_of_seats': 200}), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['name'], 'Test Hall 2')
         self.assertEqual(response.data['number_of_seats'], 200)
 
-    def test_halls_detail_view_patch(self):
-        data = {
-            'name': 'C583',
-            'number_of_seats': 500
-        }
-        response = self.client.patch(self.detail_url, json.dumps(data), content_type='application/json', HTTP_AUTHORIZATION=self.token)
+    def test_update_hall(self):
+        hall_data = {'name': 'T blok 331', 'number_of_seats': 500}
+        response = self.client.put(self.update_delete_url, json.dumps(hall_data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_delete_hall(self):
+        response = self.client.delete(self.update_delete_url)
+        self.assertEqual(204, response.status_code)
 
-    def test_halls_detail_view_delete(self):
-        response = self.client.delete(self.detail_url, HTTP_AUTHORIZATION=self.token)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
